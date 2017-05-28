9ee4c20411c9ffae47eaab21c4bbfb9e1fff2be7
ypan887 <ypan887@gmail.com>
2017-05-28 16:00:12 -0700

    add some test

--- spec/normalize_path_spec.rb
+++ spec/normalize_path_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper.rb'
 require '../normalize_path.rb'
 
 Rspec.describe "normalize path" do
-  
+
   let test_path1 = "foo/./bar"
   let test_path2 = "foo/bar/../baz"
+  let test_path3 = "..foo/bar../baz"
+  let test_path4 = "foo//bar"
 
   it "removed sigle dot from path" do
     expect(normalize(test_path1)).to eq('foo/bar')
@@ -12,5 +14,11 @@ Rspec.describe "normalize path" do
   
   it "remove double dot from path" do
     expect(normalize(test_path2)).to eq('foo/baz')
+    expect(normalize(test_path3)).to eq('../foo/baz')
   end
+
+  it "will not perform any other normalization" do
+    expect(normalize(test_path4)).to eq('../foo/baz')
+  end
+
 end
